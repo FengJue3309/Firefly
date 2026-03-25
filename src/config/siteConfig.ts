d73494acf1225ea6d081851e034cd4823c803526
枫诀 <admin@blog.v7w.cn>
2026-03-25 18:23:40 +0800

    更新站点配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -82,7 +82,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "枫诀宝贝",
+		title: "枫诀网络科技工作室",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
